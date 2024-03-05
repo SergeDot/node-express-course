b5f3320426276b543b9d88000397601ce493ba63
SergeDot <srg@dr.com>
2024-03-05 18:18:44 -0500

    resolved conflicts

--- 01-node-tutorial/answers/12-http.js
+++ 01-node-tutorial/answers/12-http.js
@@ -19,19 +19,6 @@ const server = http.createServer((req, res) => {
   };
 });
 
-/*
-const server = http.createServer(function (req, res) {
-  if (req.url === '/'){
-    res.end('Welcome to our home page')
-  }
-  if (req.url === '/about'){
-    res.end('Here is our short history')
-  }
-  res.end(`<h1>Oops!</h1>
-  <p>We can't seems to find the page you are looking for</p>
-  <a href="/">back home</a>`)
-})
-*/
 server.listen(port, () => {
   console.log(`The server is listening on port ${port}.`);
 });
